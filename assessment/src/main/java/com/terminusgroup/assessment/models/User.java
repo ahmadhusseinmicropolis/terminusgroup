2c92e6961a1f4787e1913deba32279633f98cc5c
ahmadhusseinmicropolis <ahmad.hussein@micropolis.ae>
2022-08-10 00:01:29 +0400

    add user entity class

---
+++ assessment/src/main/java/com/terminusgroup/assessment/models/User.java
@@ -0,0 +1,69 @@
+package com.terminusgroup.assessment.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+    private String fisrtName;
+	
+	@Column
+    private String lastName;
+	
+	@Column
+    private String occupation;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFisrtName() {
+		return fisrtName;
+	}
+
+	public void setFisrtName(String fisrtName) {
+		this.fisrtName = fisrtName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Column
+    private int age;
+    
+	
+}

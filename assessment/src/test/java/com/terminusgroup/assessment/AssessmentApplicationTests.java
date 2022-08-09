a7db0c70ac068b436ce6b7973811b120da19e980
ahmadhusseinmicropolis <ahmad.hussein@micropolis.ae>
2022-08-09 23:59:10 +0400

    init the project and set the database configurations

---
+++ assessment/src/test/java/com/terminusgroup/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminusgroup.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
